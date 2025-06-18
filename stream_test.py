8ed274136ca371d104953eb6ecab06f8ab8e733d
abe-mart <19436449+abe-mart@users.noreply.github.com>
2025-06-17 18:39:20 -0600

    Update stream_test.py

--- stream_test.py
+++ stream_test.py
@@ -188,4 +188,4 @@ if uploaded_file is not None:
     st.download_button('Down It',data=zip_buffer,file_name=filename_up+'_listing_images.zip',mime="application/zip")
 
 
-st.link_button('Video Mockup Creator', url='https://coralvideo.streamlit.app/', icon='video_camera_front')
+st.link_button('Video Mockup Creator', url='https://coralvideo.streamlit.app/')
